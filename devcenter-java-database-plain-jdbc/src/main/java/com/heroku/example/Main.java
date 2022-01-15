1d5f6db42e48aaafb74b393577aad4dddb323d85
virkumar <virkumar@microsoft.com>
2022-01-15 12:18:04 +0530

    dd

--- devcenter-java-database-plain-jdbc/src/main/java/com/heroku/example/Main.java
+++ devcenter-java-database-plain-jdbc/src/main/java/com/heroku/example/Main.java
@@ -28,7 +28,7 @@ public class Main {
 //		String dbUrl = "jdbc:postgres://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 		
 //		String dbUrl ="jdbc:postgresql://elrwexgnaixdbs:a12f909465266552bb23e17d6087dc88e3e3286d2f501402a5e580dfd59da263@ec2-3-232-22-121.compute-1.amazonaws.com:5432/da5nf2bf0co74q";
-		System.out.println(dbUri);
+		System.out.println(dbUri.toString());
 		
 		return DriverManager.getConnection(dbUri);
 	}
